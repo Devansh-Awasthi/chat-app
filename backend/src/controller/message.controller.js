2e18f05fd49a20e69db20b7b2727b91a1af0631f
Devansh-Awasthi <devanshawasthi03@gmail.com>
2025-06-21 17:05:34 +0530

    message route done

--- backend/src/controller/message.controller.js
+++ backend/src/controller/message.controller.js
@@ -1,6 +1,6 @@
 import Message from "../model/db.message";
 import User from "../model/db.user";
-
+import cloudinary from "../lib/cloudinary.js";
 export const getUserForSidebar = async(req,res) =>{
  try {
     const loggedInUser = req.user._id;
@@ -22,10 +22,41 @@ export const getMessage= async(req,res) =>{
                {senderId:userToChat,receiverId:myId} 
             ]
         })
+        res.status(200).json(messages);
     } catch (error) {
        console.log('Internal sever Error getMessage:', error);
       res.status(500).json({message:'An Error Occured'});
     
         
     }
+}
+
+export const sendMessage= async(req,res) =>{
+   try {
+    const {image,text} = req.body;
+    const {id:receiverId} = req.params
+    const myId = req.user._id;
+    let imgUrl ;
+    if(image){
+        const uploadResponse = await cloudinary.uploader.upload(image);
+        imgUrl = uploadResponse.secure_url;
+    }
+    const newMessage =new Message({
+        senderId:myId,
+        receiverId:id,
+        text:text,
+        image:imgUrl
+
+    })
+await newMessage.save();
+
+
+
+res.status(201).json(newMessage)
+   } catch (error) {
+      console.log('Internal sever Error sendMessage:', error);
+      res.status(500).json({message:'An Error Occured'});
+    
+
+   } 
 }
--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { checkProtect } from '../middleware/auth.checkProtect.js';
-import { getMessage, getUserForSidebar } from '../controller/message.controller.js';
+import { getMessage, getUserForSidebar, sendMessage } from '../controller/message.controller.js';
 const router = express.Router();
 router.get('/user' , checkProtect , getUserForSidebar);
 router.get('/:id' , checkProtect , getMessage);
+router.post('/send/:id' , checkProtect , sendMessage);
 export default router;
