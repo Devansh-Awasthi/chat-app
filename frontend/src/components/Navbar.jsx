e35c9a376a6ea362c92742d4e30b4ed3c5cfec6f
Devansh-Awasthi <devanshawasthi03@gmail.com>
2025-06-28 12:46:53 +0530

    setting completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
 
           <div className="flex items-center gap-2">
             <Link
-              to={"/settings"}
+              to={"/setting"}
               className={`
               btn btn-sm gap-2 transition-colors
               
---
+++ frontend/src/store/useMessageStore.js
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import { toast } from "react-hot-toast";
+export const useMessageStore = create((stat) => ({
+  isMessageLoading: false,
+  isUserLoading: false,
+  selectedUser: null,
+  messages: [],
+  users: [],
+  getUser: async () => {
+    stat({ isUserLoading: true });
+    try {
+      const backendHit = await axiosInstance.get("/message/user");
+      stat({ users: backendHit.data });
+    } catch (error) {
+      toast.error("getUser Failed ", error.response.data.message);
+      console.log(" Error in getUser:", error);
+    } finally {
+      stat({ isUserLoading: false });
+    }
+  },
+  getMessage:async (userId) => {
+    stat({ isMessageLoading: true });
+    try {
+      const backendHit = await axiosInstance.get(`/message/${userId}`);
+      stat({ messages: backendHit.data });
+    } catch (error) {
+      toast.error("getMessage Failed ", error.response.data.message);
+      console.log(" Error in getUser:", error);
+    } finally {
+      stat({ isMessageLoading: false });
+    }
+  }
+}));
